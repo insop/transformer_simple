b1ecc3f494f4e743107b74b6ee6fbe5a91ff7fb2
Insop Song <insop.song@gmail.com>
2020-12-03 19:40:51 -0800

    wip

--- src/python/classifier.py
+++ src/python/classifier.py
@@ -15,7 +15,7 @@ from transformer_simple import TransformerBlock_naive
 
 ## for classify
 class TransformerSimpleClassify(nn.Module):
-    def __init__(self, n_seq, dim_emb, dim_internal, num_tokens, num_classes, max_pool=True, heads=8, depth=6, mask=False, ff_hidden_mult=4, dropout=0.0):
+    def __init__(self, n_seq, dim_emb, dim_internal, num_tokens, num_classes, max_pool=True, heads=8, depth=6, mask=False, ff_hidden_mult=4, dropout=0.0, dtype=torch.float32):
         super().__init__()
 
         self.num_tokens = num_tokens
@@ -24,17 +24,17 @@ class TransformerSimpleClassify(nn.Module):
         self.dim_emb = dim_emb
         self.dim_internal = dim_internal
 
-        self.token_embedding = nn.Embedding(embedding_dim=dim_emb, num_embeddings=num_tokens)
-        self.pos_embedding = nn.Embedding(embedding_dim=dim_emb, num_embeddings=n_seq)
+        self.token_embedding = nn.Embedding(embedding_dim=dim_emb, num_embeddings=num_tokens).type(dtype)
+        self.pos_embedding = nn.Embedding(embedding_dim=dim_emb, num_embeddings=n_seq).type(dtype)
 
-        trfm_blocks = [TransformerBlock_naive(n_seq=n_seq, dim_emb=dim_emb, dim_internal=dim_emb, heads=heads) \
+        trfm_blocks = [TransformerBlock_naive(n_seq=n_seq, dim_emb=dim_emb, dim_internal=dim_emb, heads=heads, dtype=dtype) \
                        for _ in range(depth)]
 
-        self.trfm_blocks = nn.Sequential(*trfm_blocks)
+        self.trfm_blocks = nn.Sequential(*trfm_blocks).type(dtype)
 
-        self.toprobs = nn.Linear(dim_emb, num_classes)
+        self.toprobs = nn.Linear(dim_emb, num_classes).type(dtype)
 
-        self.do = nn.Dropout(dropout)
+        self.do = nn.Dropout(dropout).type(dtype)
 
     def forward(self, x):
 
